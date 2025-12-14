5ee8af1ac0916c7097c1a21868a2894ca3adeb02
UnaTried <103455203+UnaTried@users.noreply.github.com>
2025-12-14 14:42:03 +0100

    Fix syntax error in tfm.lua

--- lua/tfm.lua
+++ lua/tfm.lua
@@ -66,7 +66,7 @@ M.FILE_MANAGERS = {
         set_focused_file = "--select",
     },
     superfile = {
-        cmd = "spf"
+        cmd = "spf",
         set_file_chooser_output = "--chooser-file",
         set_focused_file = "",
     }
